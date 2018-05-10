20e05e0f7081503ef2a291791f50af3e42febf74
chirale <chirale@gmail.com>
2018-05-10 15:59:55 +0200

    Autocompletamento luoghi su TypeAhead: prima implementazione

--- routes.js
+++ routes.js
@@ -1,27 +1,30 @@
 var express = require('express');
 var request = require('request');
-const wikiDataBaseQueryUrl =  "https://query.wikidata.org/sparql?query=";
+const WIKIDATABASEQUERYURL =  "https://query.wikidata.org/sparql?query=";
 // Elenca i comuni da visualizzare nell'autocomplete
-const querySPARQLComuniAutocomplete = `SELECT DISTINCT ?comune ?comuneLabel
+const QUERY_SPARQL_COMUNI_AUTOCOMPLETE = `SELECT DISTINCT ?comune ?comuneLabel
 WHERE {
 ?idWD wdt:P131 ?comune . ?comune wdt:P17 wd:Q38 .
 { ?idWD wdt:P2186 ?idWLM . } UNION
 { ?idWD wdt:P1435 wd:Q26971668 . }
 
 SERVICE wikibase:label { bd:serviceParam wikibase:language "it". }
-}`;
-const querySPARQLComuniAutocompleteEncoded = wikiDataBaseQueryUrl + encodeURIComponent(querySPARQLComuniAutocomplete);
+}
+ORDER BY ASC (?comuneLabel)`;
+const QUERY_SPARQL_COMUNI_AUTOCOMPLETE_URL = WIKIDATABASEQUERYURL + encodeURIComponent(QUERY_SPARQL_COMUNI_AUTOCOMPLETE);
 
 module.exports = function(app, apicache, passport) {
+    // file statici e index.html in app/
+    app.use('/', express.static('./app'));
     // app.get(/comuni\/(.+)$/, apicache('2 seconds'), function(req, res) {
-    app.get(/comuni\.json$/, apicache('1 day'), function(req, res) {
+    app.get(/comuni\.json$/, apicache('1 second'), function(req, res) {
         try {
             let querypara = req.params[0];
             let jsonRes = [];
             let options = {
-                url: querySPARQLComuniAutocompleteEncoded,
+                url: QUERY_SPARQL_COMUNI_AUTOCOMPLETE_URL,
                 headers: {
-                  'Accept': 'application/json'
+                    'Accept': 'application/json'
                 }
             };
 
@@ -30,8 +33,21 @@ module.exports = function(app, apicache, passport) {
                     console.log('errore richiesta comuni');
                 }
                 else {
-                    var results = JSON.parse(body).results.bindings;
-                    // console.log(results);
+                    // return JSON.parse(body).results.bindings;
+                    var results = [];
+                    // Elaboro i risultati in modo da renderli consumabili
+                    // da typeahead
+                    var parsed = JSON.parse(body).results.bindings;
+                    for (var i = 0; i < parsed.length; i++) {
+                        // results.push(parsed[i].comuneLabel.value);
+                        if (i == 0) {
+console.log(parsed[i]);
+                        }
+                        results.push({
+                          'wdid':parsed[i].comune.value,
+                          'label':parsed[i].comuneLabel.value
+                        });
+                    }
                     res.send(results);
                 }
             });
