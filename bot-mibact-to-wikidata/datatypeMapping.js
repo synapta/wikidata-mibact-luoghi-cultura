2031a561663aeb6e5102b0ec9563222f610305c8
alemela <alessiom92@gmail.com>
2018-05-18 12:51:27 +0200

    added mapping for instance of

---
+++ bot-mibact-to-wikidata/datatypeMapping.js
@@ -0,0 +1,27 @@
+var instanceOf = {
+    "CHIESA": "Q16970",
+    "MUSEO ARCHEOLOGICO": "Q3329412",
+    "MUSEO": "Q33506",
+    "PALAZZO": "Q16560",
+    "ORATORIO": "Q580499",
+    "CAPPELLA": "Q108325",
+    "PIEVE": "Q1464870",
+    "PINACOTECA": "Q740437",
+    "TEATRO": "Q24354",
+    "PORTA": "Q82117",
+    "BIBLIOTECA": "Q7075",
+    "BASILICA": "Q163687",
+    "MONUMENTO": "Q4989906"
+}
+
+exports.mapTypeFromLabel = function (label) {
+    let l = label.toUpperCase();
+
+    Object.keys(instanceOf).forEach(function(key, index) {
+        if (l.startsWith(key)) {
+            return instanceOf[key];
+        }
+    });
+
+    return undefined;
+}
